22eac32f931cd19a409eafbe1f948539ccdb9a4f
LPMartinez <lpmartinez1109@gmail.com>
2020-02-19 18:13:27 -0600

    Updates

---
+++ precache-manifest.4e6fbd74f7cc548f633a314ccafd289b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "312cf9c4d64524b299068d142a8f7e18",
+    "url": "/employee_management_app/index.html"
+  },
+  {
+    "revision": "faa8869853d335a715d7",
+    "url": "/employee_management_app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a8f28f61e52182808f17",
+    "url": "/employee_management_app/static/js/2.ae550a9f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/employee_management_app/static/js/2.ae550a9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faa8869853d335a715d7",
+    "url": "/employee_management_app/static/js/main.a8980eeb.chunk.js"
+  },
+  {
+    "revision": "02c5fb25f0c812b20042",
+    "url": "/employee_management_app/static/js/runtime-main.0032e351.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/employee_management_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
